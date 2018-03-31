f9c9ffd9a9d7d775be71347dfdc7d3e84663add7
Jakob Klinger <klinger-jakob@t-online.de>
2018-03-31 20:49:31 +0200

    Ausbau der Grundklassen

--- src/basic/Spieler.java
+++ src/basic/Spieler.java
@@ -1,7 +1,9 @@
 package basic;
+import java.util.*;
 
 public class Spieler {
 	public String name; 
+	public ArrayList<Land> countries = new ArrayList<>();
 	
 	public Spieler(String name){
 		this.name = name; 
--- src/basic/Welt.java
+++ src/basic/Welt.java
@@ -4,12 +4,21 @@ public class Welt {
 	
 	private int hoehe; 
 	private int breite; 
+	public int spielerZahl; 
 	public Land[] countries; 
 	
-	public Welt(int x, int y){
+	public Welt(int x, int y, int spieler){
 		this.hoehe = y; 
 		this.breite = x; 
+		spielerZahl = spieler; 
+		
+		initialisieren(); 
 	}
+	
+	private void initialisieren() {
+		
+		
+	}	
 	
 
 }
